bc0de790def592976d4e2bb90d08018f630cd358
Dave <db001@users.noreply.github.com>
2018-11-28 11:31:32 +0000

    Update codeSnippets.js

--- codeSnippets.js
+++ codeSnippets.js
@@ -276,3 +276,30 @@ function optiSideScroll(element, direction, speed, distance, step) {
         }
     }, speed);
 }
+
+// Debounce function and usage
+function optiDebounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+};
+
+var eventFunction = optiDebounce(function () {
+    // Do something
+}, 250);
+
+element.addEventListener(event, eventFunction);
