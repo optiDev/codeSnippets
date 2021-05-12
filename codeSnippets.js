d8c569ae15398b1d54f5e246a53b16304bff7631
Dave <db001@users.noreply.github.com>
2021-05-12 14:26:45 +0100

    Update codeSnippets.js

--- codeSnippets.js
+++ codeSnippets.js
@@ -236,7 +236,7 @@ var teliumWait = setInterval(function() {
     if (myButton && window.utag && utag.link) {
         clearInterval(teliumWait);
         myButton.addEventListener('click', function() {
-            utag.link({ eventAction: "Optimisation Event", eventCategory: "Optimisation", eventName: "Random test event name", event: 'tiq.on'})
+            utag.link({ eventAction: "Optimisation Event", eventCategory: "Optimisation", eventName: "Random test event name", event: 'tiq.on'}, "", [6])
         });
     }
 },300);
